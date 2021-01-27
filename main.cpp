8d93a95ced628002f871cc86185eb0154a40195c
Josef Koch <josef.koch97@gmail.com>
2021-01-27 14:39:34 +0100

    implemented loading of dds textures and finished up project

--- main.cpp
+++ main.cpp
@@ -1,19 +1,20 @@
-#include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
-#include <glm/gtc/type_ptr.hpp>
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
 #include "shader/shader.h"
 #include <iostream>
 #include <map>
 #include <vector>
+#include <cstring>
 
-#define STB_IMAGE_IMPLEMENTATION
-#include "texture/stb_image.h"
+#define FOURCC_DXT1 0x31545844 // Equivalent to "DXT1" in ASCII
+#define FOURCC_DXT3 0x33545844 // Equivalent to "DXT3" in ASCII
+#define FOURCC_DXT5 0x35545844 // Equivalent to "DXT5" in ASCII
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow *window);
-unsigned int loadTexture(const char *path);
+unsigned int loadCompressedTexture(const char *path);
 
 // settings
 const unsigned int SCR_WIDTH = 1280;
@@ -198,13 +199,8 @@ int main()
 
 
     // load and create a textures
-    unsigned int cubeTexture = loadTexture("../texture/stones.jpg");
-    unsigned int transparentTexture = loadTexture("../texture/window.png");
-
-    // tell opengl for each sampler to which cubeTexture unit it belongs to (only has to be done once)
-    // -------------------------------------------------------------------------------------------
-    //ourShader.use();
-    //ourShader.setInt("ourTexture", 0);
+    unsigned int cubeTexture = loadCompressedTexture("../texture/stones.dds");
+    unsigned int transparentTexture = loadCompressedTexture("../texture/window.dds");
 
 
     // render loop
@@ -225,9 +221,6 @@ int main()
         // ------
         glClearColor(0.2f, 0.2f, 0.2f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // also clear the depth buffer now!
-
-        // activate texture
-        glActiveTexture(GL_TEXTURE0);
 
         // activate shader
         ourShader.use();
@@ -251,6 +244,8 @@ int main()
             // calculate the model matrix for each object and pass it to shader before drawing
             glm::mat4 model = glm::mat4(1.0f);
             model = glm::translate(model, cubePositions[i]);
+
+            // rotate the cubes in the back
             if (cubePositions[i].z < -7)
             {
                 float angle = 20.0f + i;
@@ -325,7 +320,6 @@ void processInput(GLFWwindow *window)
 }
 
 // glfw: whenever the window size changed (by OS or user resize) this callback function executes
-// ---------------------------------------------------------------------------------------------
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
     // make sure the viewport matches the new window dimensions; note that width and
@@ -333,127 +327,69 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height)
     glViewport(0, 0, width, height);
 }
 
-// utility function for loading a 2D texture from file
-// ---------------------------------------------------
-unsigned int loadTexture(char const * path)
-{
+// load compressed dds texture from file
+unsigned int loadCompressedTexture(const char *path) {
+    unsigned char header[124];
+
+    FILE *fp;
+
+    /* try to open the file */
+    fp = fopen(path, "rb");
+    if (fp == NULL)
+        return 0;
+
+    fseek(fp, 0, SEEK_END);
+    long file_size = ftell(fp);
+    fseek(fp, 0, SEEK_SET);
+
+    /* verify the type of file */
+    char filecode[4];
+    fread(filecode, 1, 4, fp);
+    if (strncmp(filecode, "DDS ", 4) != 0) {
+        fclose(fp);
+        return 0;
+    }
+
+    /* get the surface desc */
+    fread(&header, 124, 1, fp);
+
+    unsigned int height = *(unsigned int*)&(header[8]);
+    unsigned int width = *(unsigned int*)&(header[12]);
+    unsigned int mipMapCount = *(unsigned int*)&(header[24]);
+    unsigned int fourCC = *(unsigned int*)&(header[80]);
+
+    unsigned char* buffer;
+    /* how big is it going to be including all mipmaps? */
+    buffer = (unsigned char*)malloc(file_size - 128);
+    fread(buffer, 1, file_size, fp);
+    /* close the file pointer */
+    fclose(fp);
+
+    unsigned int components  = (fourCC == FOURCC_DXT1) ? 3 : 4;
+    unsigned int format;
+    switch(fourCC)
+    {
+        case FOURCC_DXT1:
+            format = GL_COMPRESSED_RGBA_S3TC_DXT1_EXT;
+            break;
+        case FOURCC_DXT3:
+            format = GL_COMPRESSED_RGBA_S3TC_DXT3_EXT;
+            break;
+        case FOURCC_DXT5:
+            format = GL_COMPRESSED_RGBA_S3TC_DXT5_EXT;
+            break;
+        default:
+            free(buffer);
+            return 0;
+    }
+
+    // Create one OpenGL texture
     unsigned int textureID;
     glGenTextures(1, &textureID);
 
-    int width, height, nrComponents;
-    unsigned char *data = stbi_load(path, &width, &height, &nrComponents, 0);
-    if (data)
-    {
-        GLenum format;
-        if (nrComponents == 1)
-            format = GL_RED;
-        else if (nrComponents == 3)
-            format = GL_RGB;
-        else if (nrComponents == 4)
-            format = GL_RGBA;
-
-        glBindTexture(GL_TEXTURE_2D, textureID);
-        glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
-        glGenerateMipmap(GL_TEXTURE_2D);
-
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, format == GL_RGBA ? GL_CLAMP_TO_EDGE : GL_REPEAT); // for this tutorial: use GL_CLAMP_TO_EDGE to prevent semi-transparent borders. Due to interpolation it takes texels from next repeat
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, format == GL_RGBA ? GL_CLAMP_TO_EDGE : GL_REPEAT);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-
-        stbi_image_free(data);
-    }
-    else
-    {
-        std::cout << "Texture failed to load at path: " << path << std::endl;
-        stbi_image_free(data);
-    }
-
-    return textureID;
-}
-
-/*
-GLuint texture_loadDDS(const char* path) {
-    // lay out variables to be used
-    unsigned char *header;
-
-    unsigned int width;
-    unsigned int height;
-    unsigned int mipMapCount;
-
-    unsigned int blockSize;
-    unsigned int format;
-
-    unsigned int w;
-    unsigned int h;
-
-    unsigned char *buffer = 0;
-
-    GLuint tid = 0;
-
-    // open the DDS file for binary reading and get file size
-    FILE *f;
-    if ((f = fopen(path, "rb")) == 0)
-        return 0;
-    fseek(f, 0, SEEK_END);
-    long file_size = ftell(f);
-    fseek(f, 0, SEEK_SET);
-
-    // allocate new unsigned char space with 4 (file code) + 124 (header size) bytes
-    // read in 128 bytes from the file
-    header = malloc(128);
-    fread(header, 1, 128, f);
-
-    // compare the `DDS ` signature
-    if (memcmp(header, "DDS ", 4) != 0)
-        goto exit;
-
-    // extract height, width, and amount of mipmaps - yes it is stored height then width
-    height = (header[12]) | (header[13] << 8) | (header[14] << 16) | (header[15] << 24);
-    width = (header[16]) | (header[17] << 8) | (header[18] << 16) | (header[19] << 24);
-    mipMapCount = (header[28]) | (header[29] << 8) | (header[30] << 16) | (header[31] << 24);
-
-    // figure out what format to use for what fourCC file type it is
-    // block size is about physical chunk storage of compressed data in file (important)
-    if (header[84] == 'D') {
-        switch (header[87]) {
-            case '1': // DXT1
-                format = GL_COMPRESSED_RGBA_S3TC_DXT1_EXT;
-                blockSize = 8;
-                break;
-            case '3': // DXT3
-                format = GL_COMPRESSED_RGBA_S3TC_DXT3_EXT;
-                blockSize = 16;
-                break;
-            case '5': // DXT5
-                format = GL_COMPRESSED_RGBA_S3TC_DXT5_EXT;
-                blockSize = 16;
-                break;
-            case '0': // DX10
-                // unsupported, else will error
-                // as it adds sizeof(struct DDS_HEADER_DXT10) between pixels
-                // so, buffer = malloc((file_size - 128) - sizeof(struct DDS_HEADER_DXT10));
-            default:
-                goto exit;
-        }
-    } else // BC4U/BC4S/ATI2/BC55/R8G8_B8G8/G8R8_G8B8/UYVY-packed/YUY2-packed unsupported
-        goto exit;
-
-    // allocate new unsigned char space with file_size - (file_code + header_size) magnitude
-    // read rest of file
-    buffer = malloc(file_size - 128);
-    if (buffer == 0)
-        goto exit;
-    fread(buffer, 1, file_size, f);
-
-    // prepare new incomplete texture
-    glGenTextures(1, &tid);
-    if (tid == 0)
-        goto exit;
-
-    // bind the texture
-    // make it complete by specifying all needed parameters and ensuring all mipmaps are filled
-    glBindTexture(GL_TEXTURE_2D, tid);
+    // "Bind" the newly created texture : all future texture functions will modify this texture
+    glBindTexture(GL_TEXTURE_2D, textureID);
+    glPixelStorei(GL_UNPACK_ALIGNMENT,1);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_BASE_LEVEL, 0);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAX_LEVEL, mipMapCount - 1); // opengl likes array length of mipmaps
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
@@ -461,36 +397,25 @@ GLuint texture_loadDDS(const char* path) {
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
 
-    // prepare some variables
+    unsigned int blockSize = (format == GL_COMPRESSED_RGBA_S3TC_DXT1_EXT) ? 8 : 16;
     unsigned int offset = 0;
-    unsigned int size = 0;
-    w = width;
-    h = height;
 
-    // loop through sending block at a time with the magic formula
-    // upload to opengl properly, note the offset transverses the pointer
-    // assumes each mipmap is 1/2 the size of the previous mipmap
-    for (unsigned int i = 0; i < mipMapCount; i++) {
-        if (w == 0 || h == 0) { // discard any odd mipmaps 0x1 0x2 resolutions
-            mipMapCount--;
-            continue;
-        }
-        size = ((w + 3) / 4) * ((h + 3) / 4) * blockSize;
-        glCompressedTexImage2D(GL_TEXTURE_2D, i, format, w, h, 0, size, buffer + offset);
+    /* load the mipmaps */
+    for (unsigned int level = 0; level < mipMapCount && (width || height); ++level)
+    {
+        unsigned int size = ((width+3)/4)*((height+3)/4)*blockSize;
+        glCompressedTexImage2D(GL_TEXTURE_2D, level, format, width, height, 0, size, buffer + offset);
+
         offset += size;
-        w /= 2;
-        h /= 2;
+        width  /= 2;
+        height /= 2;
+
+        if(width < 1) width = 1;
+        if(height < 1) height = 1;
     }
-    // discard any odd mipmaps, ensure a complete texture
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAX_LEVEL, mipMapCount - 1);
-    // unbind
+    free(buffer);
+
     glBindTexture(GL_TEXTURE_2D, 0);
 
-    // easy macro to get out quick and uniform (minus like 15 lines of bulk)
-    exit:
-    free(buffer);
-    free(header);
-    fclose(f);
-    return tid;
+    return textureID;
 }
-*/
